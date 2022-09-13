0715d94dab054b056ee9991807c771d2893aabae
gamma032steam <gamma032@gmail.com>
2022-09-13 23:51:30 +1000

    Add remaining letter values

--- letter.py
+++ letter.py
@@ -1,6 +1,7 @@
 class Letter:
     '''Represents a letter on the game board'''
-    char_to_points = {'a':1,'c':5,'d':3,'e':1,'g':3,'i':1,'k':6,'l':3,'n':2,'o':1,'p':4,'r':2,'s':2,'t':2,'x':7,'y':4,'z':8}
+    char_to_points = {'a':1,'b':4,'c':5,'d':3,'e':1,'f':5,'g':3,'h':4,'i':1,'j':7,'k':6,'l':3,'m':4,'n':2,'o':1,'p':4,'q':8,'r':2,'s':2,'t':2,'u':4,'v':5,'w':5,'x':7,'y':4,'z':8}
+    assert(len(char_to_points) == 26)
 
     def __init__(self, char: str, diamonds: int, multiplier: int, does_double_word: bool, position: tuple, swapped_letter: bool):
         self.char = char
